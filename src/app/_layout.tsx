a41001f8c8e6eac7bdc43effa523dfcc1744fc4e
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-06-18 15:09:19 +0200

    add strict mode

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 
@@ -5,39 +7,41 @@ import { Colors } from '@/constants/theme'
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="dark" />
-      <Stack
-        screenOptions={{
-          headerShadowVisible: false,
-          headerBackButtonDisplayMode: 'minimal',
-          headerTintColor: Colors.default.text,
-          headerStyle: {
-            backgroundColor: Colors.default.background,
-          },
-          headerTitleStyle: {
-            color: Colors.default.text,
-          },
-          contentStyle: {
-            backgroundColor: 'transparent',
-          },
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: 'LarParty' }} />
-        <Stack.Screen name="party/new" options={{ title: 'New Party' }} />
-        <Stack.Screen
-          name="party/[partyId]"
-          options={{ title: 'Party Details' }}
-        />
-        <Stack.Screen
-          name="party/[partyId]/card/new"
-          options={{ title: 'New Character Card' }}
-        />
-        <Stack.Screen
-          name="party/[partyId]/card/[cardId]"
-          options={{ title: 'Character Card' }}
-        />
-      </Stack>
-    </>
+    <React.StrictMode>
+      <>
+        <StatusBar style="dark" />
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+            headerBackButtonDisplayMode: 'minimal',
+            headerTintColor: Colors.default.text,
+            headerStyle: {
+              backgroundColor: Colors.default.background,
+            },
+            headerTitleStyle: {
+              color: Colors.default.text,
+            },
+            contentStyle: {
+              backgroundColor: 'transparent',
+            },
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: 'LarParty' }} />
+          <Stack.Screen name="party/new" options={{ title: 'New Party' }} />
+          <Stack.Screen
+            name="party/[partyId]"
+            options={{ title: 'Party Details' }}
+          />
+          <Stack.Screen
+            name="party/[partyId]/card/new"
+            options={{ title: 'New Character Card' }}
+          />
+          <Stack.Screen
+            name="party/[partyId]/card/[cardId]"
+            options={{ title: 'Character Card' }}
+          />
+        </Stack>
+      </>
+    </React.StrictMode>
   )
 }
